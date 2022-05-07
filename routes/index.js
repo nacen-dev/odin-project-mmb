832c6c9085d904f8574bd3c7c27987c0b3bfceda
nacen <nacen.dev@gmail.com>
2022-05-08 01:19:14 +0800

    feature: create route for new message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages });
+});
+
+router.post("/new", (req, res, next) => {
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  };
+  messages.push(newMessage)
+  res.redirect("/")
 });
 
 module.exports = router;
